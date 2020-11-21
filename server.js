0261f8ef61616b4f0825086e5541aa81bf619dc9
Mike Olson <mikeolson@mikes-mbp.myfiosgateway.com>
2020-11-21 12:44:48 -0500

    adds boilerplate to server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// require the modules and initialize settings variables
+const express = require('express');
+const port = 3000;
+
+const morgan = require('morgan');
+
+// create the express app
+const app = express();
+
+// configure server settings
+require('./config/database');
+
+app.set('view engine', 'ejs');
+
+// mount middleware
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+// mount routes
+
+// tell app to listen 
+app.listen(port, function() {
+    console.log(`Express is listening on port:${port}`);
+});
